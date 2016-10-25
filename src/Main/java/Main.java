fb68e87ef4cc9c05bf2c618595da6f8fd47f0bdc
Nilmor19 <Morriskeehnen@gmail.com>
2016-10-25 12:14:30 +0200

    efwfwe1wq22q

--- src/Main/java/Main.java
+++ src/Main/java/Main.java
@@ -6,6 +6,7 @@ import com.sun.org.apache.xerces.internal.parsers.BasicParserConfiguration;
 import static spark.Spark.*;
 
 public class Main {
+
     BasicConfigurator.configure()
     public static void main(String[] args) {
     get("/hello", (req, res) -> "Hello World");
