dcb140e90de402503f319de0d3e6858ed0fcea29
Nilmor19 <Morriskeehnen@gmail.com>
2016-10-25 12:07:03 +0200

    efwfwe1wq22q

---
+++ src/Main/java/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by nilmor on 10/25/2016.
+ */
+import com.sun.org.apache.xerces.internal.parsers.BasicParserConfiguration;
+
+import static spark.Spark.*;
+
+public class Main {
+    BasicConfigurator.configure()
+
+    public static void main(String[] args) {
+    get("/hello", (req, res) -> "Hello World");
+}
+}
+
+
