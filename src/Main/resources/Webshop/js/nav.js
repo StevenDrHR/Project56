1c88000c87d6928465096ac6da89ea34bb84e0ce
Shervin <0897669@hr.nl>
2017-01-16 14:35:21 +0100

    added restriction

---
+++ src/Main/resources/Webshop/js/nav.js
@@ -0,0 +1,9 @@
+document.write('<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">');
+document.write('<ul class="nav navbar-nav">');
+document.write('<li id="profilevisibility"><a href="http://localhost:4567/Profile">Profile</a></li>');
+document.write('<li id="registervisibility"><a href="http://localhost:4567/Register">Register</a></li>');
+document.write('<li id="adminvisibility"><a href="http://localhost:4567/Adminpage">Adminpage</a></li>');
+document.write('</ul>');
+document.write('</div>');
+document.write('</div>');
+
