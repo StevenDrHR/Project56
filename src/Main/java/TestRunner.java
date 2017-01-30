4a7088705600f6bbf4e352873008666b0292beec
StevenDrHR <0906986@hr.nl>
2017-01-30 15:25:40 +0100

    Extend TestCase

--- src/Main/java/TestRunner.java
+++ src/Main/java/TestRunner.java
@@ -1,7 +1,8 @@
+import junit.framework.TestCase;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-public class TestRunner {
+public class TestRunner extends TestCase {
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(TestJUnit.class);
 
